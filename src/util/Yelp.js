653c67741c97b9d7aa538c8d59753a403c73b3a8
SJammy <SJammy@users.noreply.github.com>
2017-11-15 18:04:24 +0000

    V4 Add Yelp Search API

---
+++ src/util/Yelp.js
@@ -0,0 +1,47 @@
+
+const clientId = 'CzfRRrd6x5xjxUyIlEKN1g';
+const secret = '8Rm5ORKYDks6ZVqQ3KFppCmzAhiPXcUeRY5FwPZwGsFITkExN89JlUlh798iKiHI';
+let accessToken;
+
+let Yelp = {
+  getAccessToken() {
+    if(accessToken) {
+      return new Promise(resolve => resolve(accessToken));
+    }
+    return fetch (`https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${secret}`, {method: 'POST'}).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      accessToken = jsonResponse.access_token;
+    });
+  },
+
+  search(term, location, sortBy) {
+    return Yelp.getAccessToken().then(() => {
+      return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+        headers: {Authorization: `Bearer ${accessToken}`}
+      });
+    }).then(jsonResponse => {
+      accessToken = jsonResponse.access_token;
+    }).then(jsonResponse => {
+      if(jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => {
+          return {
+            id: business.id,
+            imageSrc: business.image_url,
+            name: business.name,
+            address: business.location.address,
+            city: business.location.city,
+            state: business.location.state,
+            zipCode: business.location.zipCode,
+            category: business.categories,
+            rating: business.rating,
+            reviewCount: business.review_count
+          }
+        });
+      }
+    })
+  }
+
+};
+
+export default Yelp;
